732d53736c5c66616233454aff27a82bf02b5975
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2021-10-15 18:29:56 -0300

    unificode 1.11f

--- part1/unificode/src/App.js
+++ part1/unificode/src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-pascal-case */
 import React, { useState } from 'react'
 import './App.css';
 
@@ -9,50 +10,57 @@ const Statistics = (props) => (
 
 )
 
+const Statics_complet = (props) => (
+  <table>
+    <tbody>
+      <Statistics title ="Good" count ={props.Values[0]}/>
+      <Statistics title ="Neutral" count ={props.Values[1]}/>
+      <Statistics title ="Bad" count ={props.Values[2]}/>
+      <Statistics title ="All" count ={props.Values[3]}/>
+      <Statistics title ="Average" count ={props.Values[4]}/>
+      <Statistics title ="Positive" count ={props.Values[5]}/>
+    </tbody>
+  </table>
+
+)
+
 const Button = ({onClick, text}) => (
   <button onClick={onClick}>{text}</button>
 )
 
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const plusGood = () => {
-    setGood(good + 1)
-  }
+  
+  const plusGood = () => setGood(good + 1);
+  const plusNeutral = () => setNeutral(neutral + 1);
+  const plusBad = () => setBad(bad + 1);
 
-  const plusNeutral = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const plusBad = () => {
-    setBad(bad + 1)
-  }
-
-  const Valor = () =>  good + neutral + bad;
-  const Average = () =>{ 
-    if(Valor() !== 0) return (good - bad) / (good + neutral + bad)
-    else return 0};
-  const Positive = () =>{ 
-    if(Valor() !== 0) return 100 / (Valor() / good)
-    else return 0;
-  }
+  const All = () =>  good + neutral + bad;
+  const Average = () => (good - bad) / (good + neutral + bad);
+  const Positive = () => 100 / (All() / good);
+  const Values = [good, neutral, bad, All(), Average().toFixed(1), Positive().toFixed(1)];
+  const Buttons = <><Button onClick={plusGood} text="good"/><Button onClick={plusNeutral} text="neutral"/>
+  <Button onClick={plusBad} text="bad"/></>;
+ 
+ if (All() === 0) return(
+  <div>
+  <h1>give feedback</h1>
+    {Buttons}
+  <h2>Statistics</h2>
+    <p>No feedback given</p>
+</div>
+ )
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onClick = {plusGood} text = "good"/>
-      <Button onClick = {plusNeutral} text = "neutral"/>
-      <Button onClick = {plusBad} text = "bad"/>
+      {Buttons}
       <h2>Statistics</h2>
-      <Statistics title ="Good" count = {good}/>
-      <Statistics title ="Neutral" count = {neutral}/>
-      <Statistics title ="Bad" count = {bad}/>
-      <Statistics title ="All" count = {Valor()}/>
-      <Statistics title ="Average" count = {Average()}/>
-      <Statistics title ="Positive" count = {Positive() + " %"}/>
+      <Statics_complet Values= {Values}/>
     </div>
   )
 }
