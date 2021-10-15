644bfe6aa50db436917ef09732ef74777b50b339
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2021-10-15 19:46:22 -0300

    anecdotes 1.12

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,49 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-/*
 
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'p', null, 'Hello world, it is ', now.toString()
-    ),
-    React.createElement(
-      'p', null, a, ' plus ', b, ' is ', a + b
-    )
+const Button = ({onClick, text}) => (
+  <div>
+    <button onClick={onClick}>{text}</button> 
+  </div>
+)
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  const Random = () => {
+    let random = Math.floor(Math.random() * 4)
+    if(selected + random < 6) return setSelected(selected + random);
+    else if(selected - random >= 0)return setSelected(selected - random);
+    else if(selected + 1 < 6)return setSelected(selected + 1);
+    else return setSelected(selected - 1)
+}
+  return (
+    <div>
+      {props.anecdotes[selected]}
+      <Button onClick={Random} text="next selected"/>
+    </div>
   )
-}*/
+}
 
-/*ReactDOM.render(
-  React.createElement(App, null),
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-)*/
+)
 
-/*El nombre de cada variale debe estar en mayuscula para que el codigo funcione y si no envuelve en div
-tambien fallara*/
-
-const Hello = (props) => {
-  return (
-    <div>
-      <p>Hello world {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const name = "Peter"
-  const age = 10
-  return (
-    <div>
-      <h1>Greetings</h1>
-      <Hello name="George" age={26 + 10}/>
-      <Hello name={name} age={age}/>
-    </div>
-  )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
